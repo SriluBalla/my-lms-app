33439983b7c34e7b5c0b48b27a412f327df44f08
sriluballa <SriluNBalla@Gmail.com>
2025-05-13 21:24:42 -0400

    Initial commit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
         keywords={['Product Owner in Test', 'Quality Assistant', 'Software testing education', 'software testing certifications']}
       />
       <h1 className="text-2xl font-bold">Home Page</h1>
+      <p>This is srilu's page</p>
     </div>
   );
 };
