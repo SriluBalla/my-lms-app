6873910b9c1f8d80fb7ba8762c0c0b691c22019d
sriluballa <SriluNBalla@Gmail.com>
2025-05-14 21:33:14 -0400

    moved paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
 import Courses from './pages/Courses';
+import Home from './pages/Home';
 
 function App() {
   return (
