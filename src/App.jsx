10a4225706e1bdb6875df9610d7774a1a2269ef1
sriluballa <SriluNBalla@Gmail.com>
2025-05-20 16:50:22 -0400

    header adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import routes from './components/Routes';
 import { supabase } from './supabaseDB';
-console.log('Supabase is ready:', supabase);
-
 
 function App() {
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-  supabase.auth.getSession().then(({ data: { session } }) => {
-    if (session) {
-      setUser(session.user);
-    }
-  });
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (session) {
+        setUser(session.user);
+      }
+    });
 
-  const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-    setUser(session?.user ?? null);
-  });
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user ?? null);
+    });
 
-  return () => listener.subscription.unsubscribe();
-}, []);
+    return () => listener.subscription.unsubscribe();
+  }, []);
 
   return (
-    <Router basename="/my-lms-app">
+    <Router>
       <Routes>
         {routes.map(({ path, element }, index) => (
           <Route key={index} path={path} element={element} />
