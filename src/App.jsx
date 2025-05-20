1a766929ea19d6be0077ccc08c6fb70cb4506d7f
sriluballa <SriluNBalla@Gmail.com>
2025-05-20 16:27:36 -0400

    registration testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import routes from './components/Routes';
 import { supabase } from './supabaseDB';
 console.log('Supabase is ready:', supabase);
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
 import TextInput from "../components/TextInput";
 import PasswordInput from "../components/PasswordInput";
